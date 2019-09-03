75bf07a625a14099081c1146e192883b2b10d798
ta08 <marl4dev@gmail.com>
2019-09-03 22:17:37 +0900

    change a large number to be readable

--- girepo/extractor.py
+++ girepo/extractor.py
@@ -32,11 +32,15 @@ def fullname_format(owner, name):
     return "{0}/{1}".format(owner, name)
 
 
+def readable_number(n):
+    return "{:,}".format(n)
+
+
 default_mapper = OrderedDict([
     ('fullname', lambda x: fullname_format(
         (x['owner']['login']), x['name'])
      ),
-    ('star', lambda x: x['stargazers_count']),
+    ('star', lambda x: readable_number(x['stargazers_count'])),
     ('star/day', lambda x: calculate_star_per(
         x['created_at'], x['updated_at'], x['stargazers_count'], day_duration=1)
      ),
